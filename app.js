d436c621139a4514796c4fa1e46b2ff7b7454dc6
Sammed Dadape <samdadape@gmail.com>
2022-09-13 16:21:02 +0530

    added functionality in app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
-
 var txtInput = document.querySelector("#txt-area")
-
 var output = document.querySelector(".output");
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text){
+    return serverUrl + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    alert("error occured", error)
+}
+
 btnTranslate.addEventListener("click", function clickHandler() {
-    output.innerText = "asasasasas: " + txtInput.value
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText;
+        })
+    .catch(errorHandler)
 
 });
 
